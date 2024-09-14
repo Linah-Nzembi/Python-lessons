dd6d74b0a8155fe192fd86e99779c73667403e78
Linah-Nzembi <nzembilinah2@gmail.com>
2024-09-14 09:22:02 +0300

    Add files via upload

---
+++ # Python-lessons.py
@@ -0,0 +1,32 @@
+# Python-lessons;
+#Variables in Python;
+Favourite_Number = 55
+print(f"My Favourite number is:",{Favourite_Number})
+
+#strings
+greeting = "Hello word!"
+print(greeting)
+
+#boolean 1
+is_python_fun =True
+print(f"Is Python fun?{is_python_fun}")
+
+#Defining a function
+def greet(name):
+    return f"Hello,{name}!"
+    
+#Using the Function
+print(greet("Linah"))
+print(greet("Declan"))
+
+#List example
+fruits = ["Banana","Cherry","Apple"]
+print(f"My Favourite fruit is{fruits[1]}")
+
+#Dictionary Example
+contacts = {
+    "Linah": "1042002",
+    "Declan": "0552021" 
+}
+print(f"Linah's phone number is{contacts['Linah']}")
+
