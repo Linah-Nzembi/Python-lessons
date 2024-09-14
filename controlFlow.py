0847d6d992ee70c9dfd9b94490f986d4847b5256
Linah-Nzembi <nzembilinah2@gmail.com>
2024-09-14 10:05:33 +0300

    Create controlFlow.py

---
+++ controlFlow.py
@@ -0,0 +1,8 @@
+temperature = 30
+if temperature > 25:
+    print("Its hot wear shorts")
+elif temperature > 15:
+    print("Its warm,Wear light Jacket")
+else:
+    print("Its  bu cold bundle up")
+    
